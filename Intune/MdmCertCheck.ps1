e15412ebe7e0cedd95305ca4cc5cc9fd1ca372d7
Morten Wiingreen <mwii@ndi-dk.com>
2026-02-15 12:11:36 +0100

    updated cert script

--- Intune/MdmCertCheck.ps1
+++ Intune/MdmCertCheck.ps1
@@ -4,11 +4,11 @@
 $cert = get-childitem -path cert:\LocalMachine\My | Where-Object {$_.Issuer -eq "Microsoft Intune MD Device CA" }
 
 if ($cert) {
-    Write-host "Intune certificate found on the device"
+    Write-host   "Intune certificate found on the device"
 
     $subject = (($cert | Select-Object Subject).Subject).TrimStart("CN=")
 
-W   rite-Host "$subject is the cert subject"
+    Write-Host "$subject is the cert subject"
 
 } else {
     Write-host "No Intune cert found."
